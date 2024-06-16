8fed9ef81b809f7386a4bd3ee1c7c4a27e2e7ce7
Tahmid-fuad <tahmidfuad18@gmail.com>
2024-06-16 18:04:45 +0600

    16/6 ....

--- loginprject/src/pages/Header.jsx
+++ loginprject/src/pages/Header.jsx
@@ -39,7 +39,7 @@ function Header() {
       {/* Navbar Start */}
       <nav className="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0">
         <a href="/" className="navbar-brand d-flex align-items-center px-4 px-lg-5">
-          <img src="assets/ete logo black.png" alt="ETE Logo" style={{ height: "100%", width: "auto" }} />
+          <img src="assets/ete logo black.png" alt="" style={{ height: "100%", width: "auto" }} />
         </a>
         <div className="navbar-nav ms-auto p-4 p-lg-0">
           <a href="/" className={getNavLinkClass("/")}>Home</a>
@@ -53,7 +53,6 @@ function Header() {
             <div className="dropdown-menu fade-up m-0">
               <a href="#" className="dropdown-item">Offered Degrees</a>
               <a href="#Notice" className="dropdown-item">Notices</a>
-              <a href="#" className="dropdown-item">Our Research</a>
               <a href="contact" className="dropdown-item">Contact</a>
             </div>
           </div>
--- loginprject/src/pages/Teacher.jsx
+++ loginprject/src/pages/Teacher.jsx
@@ -73,10 +73,94 @@ function Teacher() {
                 </tbody>
               </table>
             </div>
-            <div className="col-2"></div>
-            <div className="col-2">
-              {/* <img src="../assets/saifulsir.jpg" alt="" className="img-fluid" /> */}
-              {/* <h5>Name of the advisor:</h5>Dr. Saiful Islam */}
+            <div className="col-4">
+              <div className='w-100 p-3 rounded bg-white'>
+                <form>
+                  <h5 className='text-center'>Input Number</h5>
+                  <div className='mb-2'>
+                    {/* <label htmlFor="batch">Batch</label> */}
+                    <select
+                      className='form-control'
+                    // value={batch}
+                    // onChange={(e) => setBatch(e.target.value)}
+                    >
+                      <option value="">Select Batch</option>
+                      <option value="19">19</option>
+                      <option value="20">20</option>
+                      <option value="21">21</option>
+                      <option value="22">22</option>
+                      <option value="23">23</option>
+                    </select>
+                    {/* {formErrors.batch && <p className="text-danger">{formErrors.batch}</p>} */}
+                  </div>
+                  <div className='mb-2'>
+                    {/* <label htmlFor="batch">Batch</label> */}
+                    <select
+                      className='form-control'
+                    // value={batch}
+                    // onChange={(e) => setBatch(e.target.value)}
+                    >
+                      <option value="">Select Course</option>
+                      <option value="301">ETE 301</option>
+                      <option value="303">ETE 303</option>
+                      <option value="305">ETE 305</option>
+                      <option value="307">ETE 307</option>
+                      <option value="309">ETE 309</option>
+                    </select>
+                    {/* {formErrors.batch && <p className="text-danger">{formErrors.batch}</p>} */}
+                  </div>
+                  <div className='mb-2'>
+                    {/* <label htmlFor="batch">Batch</label> */}
+                    <select
+                      className='form-control'
+                    // value={batch}
+                    // onChange={(e) => setBatch(e.target.value)}
+                    >
+                      <option value="">Exam</option>
+                      <option value="ct1">CT-1</option>
+                      <option value="ct2">CT-2</option>
+                      <option value="ct3">CT-3</option>
+                      <option value="ct4">CT-4</option>
+                      <option value="ct5">CT-5</option>
+                      <option value="term">Term Final</option>
+                    </select>
+                    {/* {formErrors.batch && <p className="text-danger">{formErrors.batch}</p>} */}
+                  </div>
+                  <div className='mb-2'>
+                    {/* <label htmlFor="id">Student ID</label> */}
+                    <input
+                      type="number"
+                      placeholder='Enter Student ID'
+                      className='form-control'
+                      // value={id}
+                      // onChange={(e) => setId(e.target.value)}
+                    />
+                    {/* {formErrors.id && <p className="text-danger">{formErrors.id}</p>} */}
+                  </div>
+                  <div className='mb-2'>
+                    {/* <label htmlFor="id">Marks</label> */}
+                    <input
+                      type="number"
+                      placeholder='Marks'
+                      className='form-control'
+                      // value={id}
+                      // onChange={(e) => setId(e.target.value)}
+                    />
+                    {/* {formErrors.id && <p className="text-danger">{formErrors.id}</p>} */}
+                  </div>
+
+                  {/* {errorMessage && <p className="text-danger">{errorMessage}</p>} */}
+                  <div className='d-grid'>
+                    <button type='submit' className='btn btn-primary mt-2'>Log in</button>
+                  </div>
+                  <p className='text-right mt-2'>
+                    {/* <Link to="/forgot-password">Forgot Password?</Link> */}
+                  </p>
+                  <div className="text-center mt-2">
+                    {/* <Link to="/signup" className='ms-2'>Sign up</Link> */}
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
@@ -87,8 +171,11 @@ function Teacher() {
             <Notice />
           </div>
           <div className="col-8">
-            
+
           </div>
+        </div>
+        <div className="row">
+
         </div>
       </div>
       <Footer />
